71baf53feaf666f907de3425fc33cc980ddcafe7
Jeremiah Hawks <mallardknundrum@gmail.com>
2017-04-11 17:04:13 -0600

    Create xcode project. Add all known files.

---
+++ Drinx/Drinx/CloudKitManager.swift
@@ -0,0 +1,9 @@
+//
+//  CloudKitManager.swift
+//  Drinx
+//
+//  Created by Jeremiah Hawks on 4/11/17.
+//  Copyright © 2017 Jeremiah Hawks. All rights reserved.
+//
+
+import Foundation
---
+++ Drinx/Drinx/CocktailTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  CocktailTableViewCell.swift
+//  Drinx
+//
+//  Created by Jeremiah Hawks on 4/11/17.
+//  Copyright © 2017 Jeremiah Hawks. All rights reserved.
+//
+
+import UIKit
+
+class CocktailTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
