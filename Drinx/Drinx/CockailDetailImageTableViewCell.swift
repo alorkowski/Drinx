8852e80d8c60bf70707dad9b265d728c7669e689
ddcornwell <ddcornwell@gmail.com>
2017-04-12 10:01:47 -0600

    add and link storyBoards

---
+++ Drinx/Drinx/CockailDetailImageTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  CockailDetailImageTableViewCell.swift
+//  Drinx
+//
+//  Created by DANIEL CORNWELL on 4/12/17.
+//  Copyright © 2017 Jeremiah Hawks. All rights reserved.
+//
+
+import UIKit
+
+class CockailDetailImageTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- Drinx/Drinx/CocktailDetailTableViewController.swift
+++ Drinx/Drinx/CocktailDetailTableViewController.swift
@@ -1,14 +1,14 @@
 //
-//  MyCabinetTableViewController.swift
+//  CocktailDetailTableViewController.swift
 //  Drinx
 //
-//  Created by Jeremiah Hawks on 4/11/17.
+//  Created by DANIEL CORNWELL on 4/12/17.
 //  Copyright © 2017 Jeremiah Hawks. All rights reserved.
 //
 
 import UIKit
 
-class MyCabinetTableViewController: UITableViewController {
+class CocktailDetailTableViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
